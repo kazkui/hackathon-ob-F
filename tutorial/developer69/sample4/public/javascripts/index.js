9747b2d23e8efcedf4a9279d18805c6a3eb4fa74
Kentairui <hrak1114@gmail.com>
2018-04-05 20:00:21 +0900

    committed in 2018/04/05 20:00:21

--- tutorial/developer69/sample4/public/javascripts/index.js
+++ tutorial/developer69/sample4/public/javascripts/index.js
@@ -1,20 +1,27 @@
 // 要素の値を取得する
 function getValue() {
-    const text1Value = '';
+    const text1Value = $('#text1').val();
     return text1Value;
 }
 
 // フォームに値が設定されている場合にポップアップを表示する
 function popupValue() {
     const text1Value = getValue();
+
+    alert(11111111);
     // ここに条件分岐を記入する
 
+    if(textValue !== ''){
+
         alert(text1Value);
+      }
 
 }
 
 // 要素に値をセットする
 function setValue() {
     const text2Value = 'Rakus!';
+
+    $('#text2').text(text2Value);
 
 }
